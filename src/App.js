0479047c7b6be9a4b026bbfa9be8c0d497987c60
muhammedanaskhan <muhammedanas5186@example.com>
2022-07-18 08:06:26 -0700

    Expense Item lists made, children props added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
-    { title: "Toilet Paper", amount: 94.12, Date: new Date(2021, 2, 21) },
-    { title: "Car Insurance", amount: 294.67, Date: new Date(2021, 2, 5) },
-    { title: "New Desk", amount: 450, Date: new Date(2021, 5, 12) },
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        ttl={expenses[0].title}
-        amt={expenses[0].amount}
-        dt={expenses[0].Date}
-      ></ExpenseItem>
-      <ExpenseItem
-        ttl={expenses[1].title}
-        amt={expenses[1].amount}
-        dt={expenses[1].Date}
-      ></ExpenseItem>
-      <ExpenseItem
-        ttl={expenses[2].title}
-        amt={expenses[2].amount}
-        dt={expenses[2].Date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
   return (
-    <div className="expense-item">
-      <div>{props.dt.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.ttl}</h2>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <div className="expense-item__price">{props.amt }</div>
-    </div>
+    </Card>
   );
 }
+
 export default ExpenseItem;
