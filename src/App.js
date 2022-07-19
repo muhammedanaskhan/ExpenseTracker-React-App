f28ded4c305c09822976af3ffb5e0abb03504d1a
muhammedanaskhan <muhammedanas5186@example.com>
2022-07-19 07:11:16 -0700

    Input form component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Expenses from './components/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
+function App() { 
   const expenses = [
     {
       id: 'e1',
@@ -25,7 +26,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
